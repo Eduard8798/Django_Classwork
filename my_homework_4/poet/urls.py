0d29113411adc73cfe5f0c4a84cef8db56bab57a
Eduard8798 <eduard9999997@gmail.com>
2025-02-13 16:53:48 +0200

    Homewrok_4_Number

--- my_homework_4/poet/urls.py
+++ my_homework_4/poet/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_poets, name='random_poem'),
+    path('randompoet/', views.random_poem, name='random_poem'),
+    path('<int:pk>/', views.delete_poem, name='delete_poem'),
     #path('/<str:author_name>/', views.random_poem_by_author, name='random_poem_by_author'),
 ]
